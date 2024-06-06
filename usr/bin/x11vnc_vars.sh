731895f4c2c86fa0de3fbd09c6e3a7b796b5727b
Ruben Cancho <304053+Canx@users.noreply.github.com>
2024-06-06 08:54:54 +0200

    Env vars in default file

--- usr/bin/x11vnc_vars.sh
+++ usr/bin/x11vnc_vars.sh
@@ -1,22 +1,24 @@
+#!/bin/bash
+
 # Encontrar todos los archivos en /var/run/sddm/
-files=$(sudo ls -tr /var/run/sddm/*)
+files=$(ls -tr /var/run/sddm/*)
 
 # Iterar sobre cada archivo encontrado
 for file in $files; do
   # Obtener el display asociado al archivo de Xauthority
-  display=$(sudo xauth -f "$file" list | awk '{print $1}' | grep -Eo ':[0-9]+$')
-  
+  display=$(xauth -f "$file" list | awk '{print $1}' | grep -Eo ':[0-9]+$')
+
   # Verificar si se encontró un display
   if [ -n "$display" ]; then
     # Establecer la variable de entorno SDDMXAUTH
-    sudo /bin/systemctl set-environment SDDMXAUTH=$file
-    
+    echo "SDDMXAUTH=$file" > /etc/default/x11vnc
+
     # Establecer la variable de entorno DISPLAY
-    export DISPLAY=$display
-    
+    echo "DISPLAY=$display" >> /etc/default/x11vnc
+
     echo "SDDMXAUTH configurado en $file"
     echo "DISPLAY configurado en $display"
-    
+
     break
   fi
 done
